309d28641cab78911901d10214148b553d326d41
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-31 20:46:26 +0300

    Added home.html and defined models and forms

---
+++ gram/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Image, Like, Comment, Profile
+
+class UserForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['editor', 'pub_date']
--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from .forms import UserForm, CommentForm
+from .models import Profile,Image, Like,Comment
+from django.contrib.auth import get_user_model
 
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+
+User = get_user_model()
+def home(request):
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        post = Image.objects.create(
+            description = data['description'],
+            image = image,
+            user = request.user
+        )
+        return redirect('home')
+    
+    all = User.objects.all()
+    images=Image.objects.all()
+
+    user = request.user
+    return render(request,'profile/account.html', {"images":images,"all":all,"user":user})
