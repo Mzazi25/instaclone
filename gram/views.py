6c8c171107a2c525532dba9fe4d74993b2aec88e
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-02 20:49:56 +0300

    Added views for the homepage

--- gram/views.py
+++ gram/views.py
@@ -9,6 +9,7 @@ User = get_user_model()
 
 
 # Create your views here.
+@login_required(login_url='accounts/login/')
 def account(request):
     if request.method == 'POST':
         data = request.POST
@@ -24,8 +25,21 @@ def account(request):
     all = User.objects.all()
     images=Image.objects.all()
 
-    user = request.user
-    return render(request,'account.html', {"images":images,"all":all,"user":user})
+    username = request.username
+    return render(request,'account.html', {"images":images,"all":all,"username":username})
+@login_required(login_url='accounts/login/')
+def home(request):
+    if request.method == 'POST':
+        data = request.POST            
+        profile = Profile.objects.create(
+            description = data['description'],
+            username = request.username,
+        )
+        return redirect('home')
+    username = request.username
+    profile = Profile.objects.filter(username=username).all()
+         
+    return render(request,'home.html',{"profile":profile})
 def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
